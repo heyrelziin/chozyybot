09503bebbee71e304c3956a353fd64c0ea3bfa65
farelfirdaus476@gmail.com <chiyofirdaus@gmail.com>
2023-04-15 12:19:32 +0700

    fisrt commit

---
+++ plugins/_antitoxic.js
@@ -0,0 +1,57 @@
+/*
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+
+export function before(m, { isBotAdmin }) {
+    if (m.isBaileys && m.fromMe) return !0
+    let chat = global.db.data.chats[m.chat]
+    let user = global.db.data.users[m.sender]
+    let isBadword = badwordRegex.exec(m.text)
+    console.log(isBadword)
+
+    if (chat.antiBadword && isBadword) {
+        user.warning += 1
+        this.sendButton(m.chat, `*📮TOXIC TERDETEKSI!!*
+あ Warning: ${user.warning} / 5 ┊
+
+[❗] Jika warning mencapai 5 kamu akan *dibanned+kick*
+
+Anda hanya bisa meminta Owner untuk membuka banned-nya !\n\n🌸 Nao-Botz
+
+“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, [['🎀CEK WARN', '.cekwarn'], ['🎐AUTHOR BOT', '.owner kontak']], m)
+        if (user.warning >= 5) {
+            user.banned = true
+            if (m.isGroup) {
+                if (isBotAdmin) {
+                	this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+                   //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
+                }
+            }
+        }
+    }
+    return !0
+}
+*/
+
+let handler = m => m
+
+let toxic = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
+handler.before = function (m, { user }) {
+  if (m.isBaileys && m.fromMe) return true
+  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
+  let chat = global.db.data.chats[m.chat]
+  let isGroupToxic = toxic.exec(m.text)
+
+  if (chat.antiToxic && isGroupToxic) {
+    m.reply('Jangan Toxic ya!!\n' + readMore + '\nMau Matikan? ketik */disable antitoxic*')
+    if (global.opts['restrict']) {
+      // if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
+  return true
+}
+
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
